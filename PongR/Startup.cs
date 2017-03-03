113c1bf8925138c49bb2da955f98fc7c28718f72
Catalin DICU <catalin.dicu@gmail.com>
2017-03-03 11:53:12 +0100

    Fixed disconnect Added LoggingPipelineModule

--- PongR/Startup.cs
+++ PongR/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Owin;
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Owin;
+using System.Diagnostics;
 
 namespace PongR
 {
@@ -7,6 +10,23 @@ namespace PongR
 		public void Configuration(IAppBuilder app)
 		{
 			app.MapSignalR();
+
+			GlobalHost.HubPipeline.AddModule(new LoggingPipelineModule());
+		}
+	}
+
+	public class LoggingPipelineModule : HubPipelineModule
+	{
+		protected override bool OnBeforeIncoming(IHubIncomingInvokerContext context)
+		{
+			Debug.WriteLine("=> Invoking " + context.MethodDescriptor.Name + " on hub " + context.MethodDescriptor.Hub.Name);
+			return base.OnBeforeIncoming(context);
+		}
+
+		protected override bool OnBeforeOutgoing(IHubOutgoingInvokerContext context)
+		{
+			Debug.WriteLine("<= Invoking " + context.Invocation.Method + " on client hub " + context.Invocation.Hub);
+			return base.OnBeforeOutgoing(context);
 		}
 	}
 }
